44c03c11d3ba831f6ea450cbf6684fc12267ce11
SoniaKarungi <98121137+SoniaKarungi@users.noreply.github.com>
2022-05-17 02:00:39 +0300

    solve linter issues

--- webpack.config.js
+++ webpack.config.js
@@ -26,16 +26,16 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-       {
-            test: /\.(png|jp(e*)g|svg)$/,  
-            use: [{
-                loader: 'url-loader',
-                options: { 
-                    limit: 8000, // Convert images < 8kb to base64 strings
-                    name: 'images/[hash]-[name].[ext]'
-                } 
-            }]
-        }
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8000, // Convert images < 8kb to base64 strings
+            name: 'images/[hash]-[name].[ext]',
+          },
+        }],
+      },
     ],
   },
 };
