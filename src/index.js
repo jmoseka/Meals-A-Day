b770ec5c9ebc5b23fcfbf59bee3f96a8d929cc4f
SoniaKarungi <98121137+SoniaKarungi@users.noreply.github.com>
2022-05-17 13:16:04 +0300

    Add get list of meals
    - JS, HTML and CSS for loading meals from external api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import './style.css';
 import Logo from '../img/logo.png';
+import getMeals from './modules/getMeals';
+import mealView from './modules/mealsView';
 
 const logo = document.getElementById('logo');
 logo.src = Logo;
+
+window.addEventListener('load', async () => {
+  const meals = await getMeals();
+  mealView(meals);
+});
